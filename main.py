7e2a1a6906f6fbb45f7c621cc6d7d942fabedc50
amaurylrd <amaurylrd@yahoo.fr>
2022-03-04 17:55:54 +0100

    Simulator class in main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
-import sys
+import sys, time
+from typing import final
 import matplotlib.pyplot as plt
 import numpy.random as random
 
@@ -13,6 +14,7 @@ class Engine(Launchable):
         pass
 
     def start(self):
+        test_loop()
         # generations paquets
         # simulation
         # plt.show()
@@ -23,39 +25,68 @@ class Engine(Launchable):
         # affiche les stats
         pass
 
-
+class Simulator(Engine):
+    def __init__(self):
+        pass
     
-# loop
-# plot graphique
-# scenario / graph
+    #package_size = 100
+    #normal_loc = 50.0
+    #normal_scale = 2.0
+    def __generate_packages(package_size : int, normal_loc=0.0, normal_scale=1.0):
+        """ Generates randomized packages from the specified parameters. 
 
+        Args:
+            package_size (int): the specified fixed size (in bits) for all packages. Must be non-negative.
+            package_loc (float, optional): the mean of the gaussian distribution. Must be non-negative. Defaults to 0.0.
+            package_spread (float, optional): the standard deviation of the gaussian distribution. Defaults to 1.0.
 
-# mean
-# scale
-
-def generate_packages():
-    packages = []
-    package_size = 10
+        Returns:
+            out (ndarray): samples of packages from the parameterized generation.
+        """
+        packages = []
+        normal_distribution = random.normal(normal_loc, normal_scale)
+        samples = int(normal_distribution)
+        
+        for _ in range(samples):
+            package = { "data": random.randint(0, 2, size=(package_size)) } # generate an n-dimensional array of random bits
+            packages.append(package)
+        
+        return packages
     
-    x = random.randint(0, 5)
-    for i in range(x):
-        package = { "data": random.randint(0, 2, size=(package_size)) } # generate an n-dimensional array of random bits
-        packages.append(package)
+    def update_simulation():
+        pass
     
-    return packages
+    def render_statistics():
+        pass
+    
 
+def test_loop():
+    TARGET_UPS = 60
+    FIXED_DELTA_TIME = 1000 / TARGET_UPS
+    MAX_ACCUMULATOR = 5 * FIXED_DELTA_TIME
+    
+    accumulator = 0
+    cycle_starting_time = time.time_ns()
+    while 1:
+        current_time = time.time_ns()
+        accumulator += (current_time - cycle_starting_time) / 1000000
+        cycle_starting_time = current_time
+        
+        if (accumulator > MAX_ACCUMULATOR):
+            accumulator = MAX_ACCUMULATOR
+        
+        while accumulator >= FIXED_DELTA_TIME:
+            #self.update(DELTA_TIME)
+            # update_simulation()
+            print("salut")
+            accumulator -= FIXED_DELTA_TIME
+            
+        # update_statistics
 
-# taille des paquets, fixe ? ou autour d'une moyenne
-# nombre de paquets
-
-
-# https://fr.khanacademy.org/computing/computer-programming/programming-natural-simulations/programming-randomness/a/normal-distribution-of-random-numbers
-# https://numpy.org/doc/stable/reference/random/generated/numpy.random.normal.html
-
-#contraintes
-#128 porteuses
-#5timeslot sur une trame
-
+        
+# Débit max = 2  W (bande passant)  log 2 V (niveau significatifs) en  bit/s
+# le bruit formule SNR
+# perte d'énergie du signal pendant sa propagation
 
 # graph -> node chainé à X node
 
@@ -78,23 +109,23 @@ def generate_packages():
 # generation des sources
 # tick en temps discret
 
-# generation paquet (hypothèse taille fixe ou ponderer la stat en fonction de la taille)
-# nombre random > Variable Bit Rate
-
 # table routage / node
 
 # statistics permanente
 
-def test():
-    print(generate_packages())
-    return 0
 
+# plt.savefig("numpy_random_numbers_stantard_normal_distribution.png", bbox_inches='tight')
+# plt.grid()
 
 
 def main(argc, kwargs):
-    # argv nolimit 0
-    print(kwargs)
-    engine = Engine() # gère des variables, avec default kwargs ?
+    # parameters: 
+    # nolimit 0
+    # npackage mean
+    
+    #print(kwargs)
+    #test()
+    engine = Simulator() # gère des variables, avec default kwargs ?
     engine.launch() # gère des variables ?
     return 0
 
