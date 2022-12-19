c674cdccdefd4901d045d82e3cf5d10779203ea5
Sourcery AI <>
2022-12-19 22:29:56 +0000

    'Refactored by Sourcery'

--- sources/engine.py
+++ sources/engine.py
@@ -24,28 +24,28 @@ class Engine(Launchable):
 
     def start(self):
         plt.style.use('bmh')
-        
+
         self._load = { 'start': 10, 'stop': 100, 'step': 5 }
-        
+
         for load in range(self._load['start'], self._load['stop'], self._load['step']):
             tick = 0
             while self._maximum_tick == 0 or tick < self._maximum_tick:
                 self.update(tick, load) # updates the simulation
                 tick += 1
-            
+
             # clears all in the simulation
             self.clear()
-            
+
             # prints the progress bar up to 95%
             progress = load / self._load['stop']
             stdout.write("\rProgress: [{0:50s}] {1:.1f}% Complete".format('#' * int(progress * 50), progress * 100))
             stdout.flush()
-        
+
         # renders the simulation and statistics
         self.render()
 
         # prints the progress bar at 100%
-        stdout.write("\rProgress: [{0:50s}] 100.0% Complete".format('#' * int(50)))
+        stdout.write("\rProgress: [{0:50s}] 100.0% Complete".format('#' * 50))
         stdout.flush()
     
     @abstractmethod
